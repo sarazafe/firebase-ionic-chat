704a6247609ad3990fd860b2129cd04161846174
sarazafe <sara@blunder.es>
2018-03-24 20:43:38 +0100

    Rename username field of user for email

--- src/model/user.ts
+++ src/model/user.ts
@@ -2,6 +2,11 @@
  * User of the chat
  */
 export class User {
-    username: string;
+    email: string;
     password: string;
+
+    constructor(email: string, password: string) {
+        this.email = email;
+        this.password = password;
+    }
 }
